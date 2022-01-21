54442870f6370a4d86bf6370af1a5247643e3bc1
Andrii <ramp8a799@gmail.com>
2022-01-22 00:08:00 +0200

    it works in some way :) (postavte 60 plz)

--- api/backend.js
+++ api/backend.js
@@ -111,15 +111,19 @@ const proxy = httpProxy.createProxyServer({
 
 export const server = http.createServer(function (req, res) {
   let origin = "https://www.mailcheck.co/";
-  if (req.url !== "/") origin = req.url.slice(1);
+  if (req.url !== "/") {
+    origin = req.url.slice(1);
+    //origin = origin.replace("./", ".//");
+    //origin[6]="//";
+    origin = origin.slice(0, 6) + "/" + origin.slice(6);
+  }
 
   proxy.on("proxyRes", function (proxyRes, req, res) {
     //proxyRes.headers["X-Frame-Options"] = "";
     proxyRes.headers["x-proxy"] = "basic-http-proxy-o4xnwnyfe-andriipolishko";
   });
-  res.end(req.url.slice(1));
-  //req.url = origin;
-  //proxy.web(req, res, { target: `${origin}`,buffer: streamify(req.rawBody) });
+
+  proxy.web(req, res, { target: `${origin}` });
 });
 
 const port = 8000;
