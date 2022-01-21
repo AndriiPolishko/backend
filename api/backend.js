a6bd09cb9b452499911b22960bc706eba167ba4a
Andrii <ramp8a799@gmail.com>
2022-01-21 21:46:32 +0200

    fixed "/" corner case

--- api/backend.js
+++ api/backend.js
@@ -112,7 +112,7 @@ const proxy = httpProxy.createProxyServer({
 
 export const server = http.createServer(function (req, res) {
   if (req.url !== "/")
-    origin = req.protocol + "://" + req.get("host") + req.url;
+    origin = /*req.protocol + "://" + req.get("host") +*/ req.url;
 
   proxy.on("proxyRes", function (proxyRes, req, res) {
     proxyRes.headers["x-proxy"] = "basic-http-proxy-o4xnwnyfe-andriipolishko";
