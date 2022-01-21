3118285ee1c1f54b04ddc6657cb355df13b6fb4a
Andrii <ramp8a799@gmail.com>
2022-01-21 22:26:51 +0200

    response check

--- api/backend.js
+++ api/backend.js
@@ -111,13 +111,14 @@ const proxy = httpProxy.createProxyServer({
 
 export const server = http.createServer(function (req, res) {
   let origin = "https://www.mailcheck.co/";
-  if (req.url !== "/")
-    origin = req.protocol + "://" + req.get("host") + req.url;
+  //if (req.url !== "/")
+  //  origin = req.protocol + "://" + req.get("host") + req.url;
 
   proxy.on("proxyRes", function (proxyRes, req, res) {
     proxyRes.headers["x-proxy"] = "basic-http-proxy-o4xnwnyfe-andriipolishko";
   });
-  proxy.web(req, res, { target: `${origin}` });
+  res.end(req.url);
+  //proxy.web(req, res, { target: `${origin}` });
 });
 
 const port = 8000;
