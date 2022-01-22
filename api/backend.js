7b7d13904c93d4ffd796f04a6f855b271af1bbde
Andrii <ramp8a799@gmail.com>
2022-01-22 10:52:32 +0200

    refactored code(postavte 60 plz)

--- api/backend.js
+++ api/backend.js
@@ -1,104 +1,3 @@
-/*let http = require("http");
-let httpProxy = require("http-proxy");
-let proxy = httpProxy.createProxyServer({});
-const https = require("https");
-
-function readURL(url) {
-  return new Promise((resolve, reject) => {
-    https
-      .get(url, (res) => {
-        // получаем статус ответа сервера посредством деструктуризации объекта
-        const { statusCode } = res;
-
-        let error;
-        if (statusCode !== 200) {
-          error = new Error(`Ошибка запроса. Код ответа: ${statusCode}`);
-        }
-
-        if (error) {
-          reject(error);
-          res.resume();
-          return;
-        }
-
-        res.setEncoding("utf8");
-
-        let rawData = "";
-        res.on("data", (chunk) => (rawData += chunk));
-
-        res.on("end", () => resolve(rawData));
-      })
-      .on("error", (e) => reject(e));
-  });
-}
-
-module.exports = async (req, res) => {
-  // res.json({ a: req.body.url });
-
-  let html;
-
-  readURL(req.body.url)
-    .then((data) => {
-      html = data;
-      console.log("Html1 - " + html);
-    })
-    .catch((err) => console.log(err.message));
-
-  res.send( typeof req.body.url);
-    //.json({ a: req.body.url });
-};
-*/
-
-/*const http = require("http");
-const httpProxy = require("http-proxy");
-
-const proxy = httpProxy.createProxyServer({
-  changeOrigin: true,
-  autoRewrite: true,
-  hostRewrite: true,
-  followRedirects: true,
-});
-
-const server = http.createServer((req, res) => {
-  let body = "";
-  req.on("data", function (chunk) {
-    body += chunk;
-  });
-
-  req.on("end", function () {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(body);
-  });
-});
-
-server.listen(8000);*/
-
-// Create a proxy server with custom application logic
-
-/*
-const proxy = httpProxy.createProxyServer({
-  changeOrigin: true,
-  autoRewrite: true,
-  hostRewrite: true,
-  followRedirects: true,
-});
-
-const server = http.createServer((req, res) => {
-  let body = "";
-  req.on("data", function (chunk) {
-    body += chunk;
-  });
-
-  req.on("end", function () {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(body);
-  });
-
-});
-
-server.listen(8000);
-*/
-
 const http = require("http");
 const httpProxy = require("http-proxy");
 
@@ -113,14 +12,13 @@ export const server = http.createServer(function (req, res) {
   let origin = "https://www.mailcheck.co/";
   if (req.url !== "/") {
     origin = req.url.slice(1);
-    //origin = origin.replace("./", ".//");
-    //origin[6]="//";
+
     origin = origin.slice(0, 6) + "/" + origin.slice(6);
   }
 
   proxy.on("proxyRes", function (proxyRes, req, res) {
-    //proxyRes.headers["X-Frame-Options"] = "";
-    proxyRes.headers["x-proxy"] = "basic-http-proxy-o4xnwnyfe-andriipolishko";
+    proxyRes.headers["X-Frame-Options"] = "";
+    //proxyRes.headers["x-proxy"] = "basic-http-proxy-o4xnwnyfe-andriipolishko";
   });
 
   proxy.web(req, res, { target: `${origin}` });
